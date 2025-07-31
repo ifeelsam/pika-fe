d206f89d33194c065eb98508f9c8c9e0e8464e35
ifeelsam <sanskarsharma9005@gmail.com>
2025-08-01 01:15:05 +0530

    style: update hover effect

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -35,7 +35,7 @@ export function Navigation() {
           className="text-2xl font-black text-pikavault-yellow sm:hover:text-white tracking-wider relative group font-monument"
         >
           PIKA
-          <span className="text-white sm:hover:text-pikavault-yellow">VAULT</span>
+          <span className="text-white sm:group-hover:text-pikavault-yellow">VAULT</span>
           <span className="absolute -bottom-1 left-0 w-0 h-[3px] bg-pikavault-yellow sm:group-hover:w-full transition-all duration-300"></span>
         </Link>
 
